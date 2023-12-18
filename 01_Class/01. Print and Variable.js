211e3d9c79133cfeec9d1baf5e0e6ea51b2171e4
Wahedul Islam <106669365+wahedpro@users.noreply.github.com>
2023-12-18 16:06:41 +0000

    Print and variable learn this class

---
+++ 01_Class/01. Print and Variable.js
@@ -0,0 +1,45 @@
+//Hello world print with my name
+console.log("Hello World");
+console.log("My Name is Wahidul Islam")
+
+//Variable in JS
+name = "wahid"
+console.log(name)
+age= 24;
+console.log(age);
+price = 99.98;
+console.log("Product Price is: "+price);
+x = null;
+console.log(x);
+y=undefined;
+console.log(y);
+
+//What is the defference between null and undefined?
+/*
+In javascript undefiend means a variable has declared but has not yet beed assigend a value
+*/
+
+//Some Variable Roles in JavaScript
+/*
+    1. Variable name is case sensitive
+    2. Only letters, digit, underscore( _ ) and $ is allowed
+    3. Only a letter, underscrope (_) or $ shoud be 1st character
+    4. Reserved words can't be variable name
+*/
+
+$name = "Islam";
+console.log($name);
+
+// let, const, var
+
+const pi = 4.1316;
+let gmail = "wahid@gmail.com";
+var valligeName = "Matar Bari";
+
+console.table([pi,gmail,valligeName]);
+
+/*
+    Prefer not to use var
+    because of issue in block scope and functional srope
+*/
+
---
+++ 01_Class/02. dataType.js
@@ -0,0 +1,1 @@
+//DataTypes in Javascript
