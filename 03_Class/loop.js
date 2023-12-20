9714e558014542ee716d2f55c826759c04451616
Wahedul Islam <106669365+wahedpro@users.noreply.github.com>
2023-12-20 15:15:55 +0000

    Learn loop in JavaScript

---
+++ 03_Class/loop.js
@@ -0,0 +1,43 @@
+console.log("This is class we are learing JavaSrcipt Loop")
+
+//print 0 to 100
+
+// for(let i=0; i<=100; i++){
+//     console.log(i);
+// }
+
+//print 0 to 100 only odd number
+
+// for(let i=0; i<=100; i++){
+//     if(i%2 != 0){
+//         console.log(i);
+//     }
+// }
+
+// Write a program to compute the sum of the first 10 natural numbers.
+
+// let sum=0;
+// for(let i=0; i<=10; i++){
+//     sum=sum+i;
+// }
+// console.log(sum);
+
+//Write a program in to display the cube of the number up to an integer.
+// let n=5;
+// for(let i=1; i<=5; i++){
+//     console.log(i+ ' Cube is: '+i*i*i);
+// }
+
+//Write a program in to display the multiplication table for a given integer.
+// let n = 10;
+// for(let i=1; i<=10; i++){
+//     console.log(n+" X "+i+" = "+n*i);
+// }
+
+//Write a program in to display a pattern like a right angle triangle using an asterisk
+// for(let i = 1; i<=5; i++){
+//     for(let j=1; j<=i; j++){
+//         console.log('* ');
+//     }
+//     console.log('\n');
+// }
