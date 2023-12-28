f562d1097ec158190bf7e266f575a684218b476b
Wahedul Islam <106669365+wahedpro@users.noreply.github.com>
2023-12-28 16:37:53 +0000

    function

---
+++ 05_Class/Functions.js
@@ -0,0 +1,20 @@
+//In this class, we are learning functions in JavaScript
+
+//Create a function to print hello world;
+
+function printHello(){
+    console.log("Hello Word");
+}
+printHello();
+
+//Write a funcion to take a name and print hello name;
+function printName(msg){
+    console.log("Hello, ",msg);
+}
+printName("Wahid");
+
+// Write a function to take two number and sum them
+function sumTwoNumber(x,y){
+    console.log(x+y);
+}
+sumTwoNumber(5,7);
