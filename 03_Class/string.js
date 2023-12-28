82f8a1e317e75dd1362b4c9302874cb4329f53b5
Wahedul Islam <106669365+wahedpro@users.noreply.github.com>
2023-12-28 10:15:12 +0000

    String learning

--- 03_Class/string.js
+++ 03_Class/string.js
@@ -1,2 +1,9 @@
 console.log("In this class we are learning String in Javascript")
 
+/*
+    It's important to know that in JavaScript, 
+    strings are immutable. This means that once a string is created, 
+    its contents cannot be changed. Instead, 
+    you must create a new string representing the modified version when 
+    you want to modify a string
+*/
